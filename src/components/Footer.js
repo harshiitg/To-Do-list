08ddaf7eb121ab88dce56c5a917d1c770a2680f8
Harsh <anandharsh186@gmail.com>
2021-06-11 12:50:24 +0530

    first

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+       <p> Designed by Harsh Anand</p>
+      
+
+        </footer>
+    )
+}
+
+export default Footer
