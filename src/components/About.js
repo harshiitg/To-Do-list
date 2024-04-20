3bf81348645cf607149284916040ca2f9bd0ebfc
harshnb <harsh.anand@nobroker.in>
2024-04-20 10:34:39 +0530

    new change

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-        <h4>Version 1.0.0</h4>
+        <h4>Version 1.0.</h4>
         <a href="/">Go Back</a> 
         </div>
     )
